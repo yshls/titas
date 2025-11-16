a870efc14449fab8b7c1b6a14fd297346436275b
yshls <shluxnsal01@gmail.com>
2025-11-16 20:53:01 +0900

    feat(tailwind): tailwind.config.js에서 색상 설정 추가 및 정리

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      black: '#333333',
+
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      // 커스텀 색상들
+      speaker1: '#C8F0EB',
+      speaker2: '#FFF4CC',
+      speaker3: '#DED9F2',
+      speaker4: '#FFEAE0',
+      primary: '#3A76F0',
+    },
     extend: {},
   },
   plugins: [],
-}
+};

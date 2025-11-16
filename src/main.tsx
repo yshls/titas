4f7aa5e232be705a3691559c0cb354c388c48314
yshls <shluxnsal01@gmail.com>
2025-11-16 22:14:29 +0900

    feat(router): react-router-dom 설정 및 페이지 라우트 정의

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { GrowthHubPage } from '@/pages/GrowthHubPage';
+import { CreatorPage } from '@/pages/CreatorPage';
+import { TalkPage } from '@/pages/TalkPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <GrowthHubPage />,
+  },
+  {
+    path: '/create',
+    element: <CreatorPage />,
+  },
+  {
+    path: '/talk/:scriptId',
+    element: <TalkPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

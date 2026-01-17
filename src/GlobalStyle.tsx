f566a54def41358006cf05f6fde0a3e6d72c6963
yshls <shluxnsal01@gmail.com>
2026-01-18 00:04:35 +0900

    feat: GlobalStyle 컴포넌트 추가 및 기본 스타일 설정

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,37 @@
+import { Global, css } from '@emotion/react';
+import { theme } from './styles/theme';
+
+const style = css`
+  body {
+    background-color: ${theme.modes.light.background};
+    color: ${theme.modes.light.text};
+
+    font-family: 'lato', sans-serif;
+    transition:
+      background-color 0.2s ease-in-out,
+      color 0.2s ease-in-out;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={style} />;
+};
+
+export default GlobalStyle;

a59f1a1cf7305ece730da25425403e7555d85a55
yshls <shluxnsal01@gmail.com>
2026-02-04 13:00:17 +0900

    feat: 로컬 개발용 SSL 인증서 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import fs from 'fs';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    https: {},
+    https: {
+      key: fs.readFileSync('./.certs/localhost-key.pem'),
+      cert: fs.readFileSync('./.certs/localhost.pem'),
+    },
     headers: {
       'Cross-Origin-Embedder-Policy': 'credentialless',
       'Cross-Origin-Opener-Policy': 'same-origin',

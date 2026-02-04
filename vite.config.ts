bd044fc99d93a429e1357284be498648ec67fd5a
yshls <shluxnsal01@gmail.com>
2026-02-04 14:04:33 +0900

    feat: import fs추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), basicSsl()],
